0b9ee77d786627999ea70217f09970e7aa70f3e4
fops777 <huzin.ayratik@gmail.com>
2023-12-29 20:00:09 +0500

    bd connected

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-
+app.use(express.json()); // позволяет читать json из запроса
 const URI = process.env.MONGODB_CONNECT_URI;
 
 mongoose
@@ -13,8 +13,30 @@ mongoose
   .then(() => console.log("DB - OK"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hey cutiee");
+const TestModel = mongoose.model("Test", {
+  title: String,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const items = await TestModel.find(); // Используем метод find() для получения всех записей
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  // res.send("Hey cutiee");
+});
+
+app.post("/register", async (req, res) => {
+  console.log(req.body.title);
+
+  const doc = new TestModel({
+    title: req.body.title,
+  });
+
+  const data = await doc.save();
+
+  res.json(data);
 });
 
 app.listen("8080", (err) => {
